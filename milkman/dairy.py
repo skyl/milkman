da7c9fd725c5674afd4cb6ce176c1176e8181676
Skylar Saveland <skylar.saveland@chase.com>
2013-02-27 17:07:28 -0500

    FileFields are null on Oracle but still need to get generated if blank.

--- milkman/dairy.py
+++ milkman/dairy.py
@@ -190,7 +190,8 @@ class MilkTruck(object):
         # In Oracle, any CharField descendant will be nullable.
         # In other databases, no CharField descendant will be nullable.
         # We can use blank as the arbiter of requiredness for CharFields.
-        if isinstance(field, models.CharField):
+        if isinstance(field, models.CharField)\
+                or isinstance(field, models.FileField):
             return not field.blank
         else:
             # generate if both blank and null are False
